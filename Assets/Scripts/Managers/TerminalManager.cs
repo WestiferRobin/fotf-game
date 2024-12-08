07cb880c253d2a90399fb3c8ab5345f3ed0203a8
Wesley  Webb <w.webb0919@gmail.com>
2024-12-08 03:40:11 -0800

    updating master

--- Assets/Scripts/Managers/TerminalManager.cs
+++ Assets/Scripts/Managers/TerminalManager.cs
@@ -1,19 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using FotF.Api.Games.BattleGames.UnitBattles;
+using FotF.Api.Terminal;
+using FotF.Terminal.Games;
 using UnityEngine;
 
 public class TerminalManager : MonoBehaviour
 {
+    public PlayerTerminal terminal;
+    public IGame game = new TrooperBattle(1);
+
     public void Awake()
     {
+        terminal = new UnityTerminal(game);
     }
 
     public void Start()
     {
-    }
-
-    public void Update()
-    {
+        terminal.Run();
     }
 }
